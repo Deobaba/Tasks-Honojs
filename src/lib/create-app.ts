3cfb2a057136bae48c322244a60abfe41bacf90e
adeola <adeola@syxlabs.com>
2024-12-03 02:58:46 +0100

    task api

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,14 +1,14 @@
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { pinoLogger } from '@/middleware/pino-logger.js';
-import { PinoLogger } from 'hono-pino';
+// import { PinoLogger } from 'hono-pino';
 
-interface AppBindings {
-    variables: {
-        logger: PinoLogger
-    }
-}
+// interface AppBindings {
+//     variables: {
+//         logger: PinoLogger
+//     }
+// }
 export default function createApp(){
-const app = new OpenAPIHono<AppBindings>({strict: false});
+const app = new OpenAPIHono({strict: false});
 
 app.use(pinoLogger());
 
